c52ccf75d60fced10f7200cc758ade00180fc140
Katie Baumgarten <catherine_baumgarten@brown.edu>
2022-04-01 14:29:40 -0400

    final updates to inter-rater reliability

--- IRCohens.py
+++ IRCohens.py
@@ -116,20 +116,24 @@ def is_in_list(num):
                     return "yes"
         return "no"
 
-#final lists (yes mean at least one overlap, no mean just put in first value from each response)
-MuskaanYN_q1 = []
-KatieYN_q1 = []   
+#final lists 
+final_list = []  
 for i in range(0, len(Katie_sorted_ids)):
     if Katie_sorted_ids[i] in Muskaan_sorted_ids:
         inputVal = is_in_list(Katie_sorted_ids[i])
         if inputVal == "no":
             #If value is "no" append False to Muskaan and True to Katie so there is a difference
-            MuskaanYN_q1.append(False)
-            KatieYN_q1.append(True)
+            final_list.append(False)
         else:
             #Append True to both Muskaan and Katie
-            MuskaanYN_q1.append(True)
-            KatieYN_q1.append(True)
+            final_list.append(True)
+
+print("proportion of overlap q1")
+proportion1 = np.mean(final_list)
+print(proportion1)
+print(f"std dev q1 {np.std(final_list)}")
+print(pd.Series(final_list).describe())
+
 
 #Q1 cohen's kappa RI
 # print("Length of overlap")
@@ -147,10 +151,10 @@ for i in range(0, len(Katie_sorted_ids)):
 # print(yes_count_q1/len(KatieYN_q1))
 # print("")
 
-obs = np.array([KatieYN_q1, MuskaanYN_q1])
-q1IR = chi2_contingency(obs)
-print("chi2 for q1")
-print(q1IR)
+# obs = np.array([KatieYN_q1, MuskaanYN_q1])
+# q1IR = chi2_contingency(obs)
+# print("chi2 for q1")
+# print(q1IR)
 
 #Q2
 Katie_sorted_ids_2 = []
@@ -167,7 +171,7 @@ q2_why_2 = 8
 q2_why_3 = 9
 
 for row in df.itertuples():
-   if (row[6] != 0 or row[q2_why_2] != 0 or row[q2_why_3] != 0):
+   if (row[q2_why_1] != 0 or row[q2_why_2] != 0 or row[q2_why_3] != 0):
         inner_list = []
         if (row[q2] != "") and (row[q2_why_1] != 0):
             inner_list.append(row[q2_why_1])
@@ -192,8 +196,6 @@ for row in df2.itertuples():
 
 
 
-MuskaanYN_q2 = []
-KatieYN_q2 = []
 Katie_sorted_ids_2.sort()
 Muskaan_sorted_ids_2.sort()
 
@@ -210,16 +212,12 @@ for i in range(0, len(Katie_sorted_ids_2)):
         Katie_final_ids.append(Katie_sorted_ids_2[i])
 
 
-Katie_final_2 = []
-Muskaan_final_2 = []
 
 def is_in_list(num):
     KatieList = Katie_q2.get(num)
     KatieList.sort()
-    Katie_final_2.append(KatieList)
     MuskaanList = Muskaan_q2.get(num)
     MuskaanList.sort()
-    Muskaan_final_2.append(MuskaanList)
         #If both inner lists are the same length
     if len(KatieList) == len(MuskaanList):
         for i in range(0, len(KatieList)):
@@ -236,38 +234,40 @@ def is_in_list(num):
     return "no"
     
 
+final_list_2 = []
 for i in range(0, len(Muskaan_final_ids)):
         inputVal = is_in_list(Katie_final_ids[i])
         if inputVal == "no":
-            MuskaanYN_q2.append(Muskaan_final_2[i][0])
-            KatieYN_q2.append(Katie_final_2[i][0])
+            final_list_2.append(False)
         else:
-            MuskaanYN_q2.append("yes")
-            KatieYN_q2.append("yes")
+            final_list_2.append(True)
 
 
 
 
-#Q2 cohen's kappa RI
+#Q2 overlap
 print("")
-print("Length of overlap Q2")
-print(len(KatieYN_q2))
+print("Proportion of overlap")
+proportion2 = np.mean(final_list_2)
+print(proportion2)
+print(f"std dev q2 {np.std(final_list_2)}")
+print(pd.Series(final_list_2).describe())
 print("")
 
-yes_count_q2 = 0
+# yes_count_q2 = 0
 
-for i in KatieYN_q2:
-    if i == "yes":
-        yes_count_q2 += 1
-
-
-print("yes proportion question 2")
-print(yes_count_q2/len(KatieYN_q2))
+# for i in KatieYN_q2:
+#     if i == "yes":
+#         yes_count_q2 += 1
 
 
-q2IR = cohen_kappa_score(KatieYN_q2, MuskaanYN_q2)
-print("kappa for q2")
-print(q2IR)
+# print("yes proportion question 2")
+# print(yes_count_q2/len(KatieYN_q2))
+
+
+# q2IR = cohen_kappa_score(KatieYN_q2, MuskaanYN_q2)
+# print("kappa for q2")
+# print(q2IR)
 
 
 #Q3
@@ -321,17 +321,11 @@ for i in range(0, len(Katie_sorted_ids_3)):
     if Katie_sorted_ids_3[i] in Muskaan_sorted_ids_3:
         Katie_final_ids_3.append(Katie_sorted_ids_3[i])
 
-
-Katie_final_3 = []
-Muskaan_final_3 = []
-
 def is_in_list(num):
     KatieList = Katie_q3.get(num)
     KatieList.sort()
-    Katie_final_3.append(KatieList)
     MuskaanList = Muskaan_q3.get(num)
     MuskaanList.sort()
-    Muskaan_final_3.append(MuskaanList)
         #If both inner lists are the same length
     if len(KatieList) == len(MuskaanList):
         for i in range(0, len(KatieList)):
@@ -348,38 +342,32 @@ def is_in_list(num):
     return "no"
     
 
+final_list_3 = []
 for i in range(0, len(Muskaan_final_ids_3)):
         inputVal = is_in_list(Katie_final_ids_3[i])
         if inputVal == "no":
-            MuskaanYN_q3.append(Muskaan_final_3[i][0])
-            KatieYN_q3.append(Katie_final_3[i][0])
+            final_list_3.append(False)
         else:
-            MuskaanYN_q3.append("yes")
-            KatieYN_q3.append("yes")
+            final_list_3.append(True)
 
 
 
 
-#Q3 cohen's kappa RI
-print("Length of overlap")
-print(len(KatieYN_q3))
-print("")
-
-yes_count_q3 = 0
-
-for i in KatieYN_q3:
-    if i == "yes":
-        yes_count_q3 += 1
+#Q3 proportion
+print("Proportion of overlap q 3")
+proportion3 = np.mean(final_list_3)
+print(proportion3)
+print(f"std dev q3 {np.std(final_list_3)}")
+print(pd.Series(final_list_3).describe())
 
 
-print("yes proportion question 3")
-print(yes_count_q3/len(KatieYN_q3))
-print("")
 
-
-q3IR = cohen_kappa_score(KatieYN_q3, MuskaanYN_q3)
-print("kappa for q3")
-print(q3IR)
+'''Recall that Muskaan and I labeled each response with a set of categories. We decided to operationalize “agreement” as 
+“do these sets share a category in common.” Thus, we decided to report inter rater 
+reliability as the proportion of  of responses with overlapping categories. 
+Using a measure such as chi-squared or cohen's kappa was difficult because we only had 
+20-100 shared responses but 20+ code options for a response.
+'''
 
 
 
